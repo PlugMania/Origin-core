e56566622f402f0cea8e623e086afb4892af4750
Ollie <dzineit@live.co.uk>
2012-08-24 10:29:25 +0200

    Formatting in Command.java
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/info/plugmania/origin_core/commands/Command.java
+++ src/info/plugmania/origin_core/commands/Command.java
@@ -5,12 +5,13 @@ import info.plugmania.origin_core.Origin_Core;
 import org.bukkit.command.CommandExecutor;
 
 public abstract class Command implements CommandExecutor {
-Origin_Core plugin;
-public Command(Origin_Core instance){
-	plugin=instance;
-}
+	Origin_Core plugin;
+	
+	public Command(Origin_Core instance) {
+		this.plugin = instance;
+	}
 
-public String getName(){
-	return this.getClass().getSimpleName();
-}
+	public String getName() {
+		return this.getClass().getSimpleName();
+	}
 }
