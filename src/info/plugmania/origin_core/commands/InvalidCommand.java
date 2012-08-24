7a930ec1ba5e12fcfd5af9fb94569076bb66b009
Ollie <dzineit@live.co.uk>
2012-08-24 10:31:17 +0200

    Formatting in InvalidCommand.java
    Also made InvalidCommand.java tell the player what they typed in case they simply made a spelling mistake in a otherwise valid command.
    
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/info/plugmania/origin_core/commands/InvalidCommand.java
+++ src/info/plugmania/origin_core/commands/InvalidCommand.java
@@ -2,19 +2,17 @@ package info.plugmania.origin_core.commands;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
 
 import info.plugmania.origin_core.Origin_Core;
 
 public class InvalidCommand extends Command {
-
 	public InvalidCommand(Origin_Core instance) {
 		super(instance);
 	}
 
-	public boolean onCommand(CommandSender sender,
-			org.bukkit.command.Command command, String something, String[] args) {
-	sender.sendMessage(ChatColor.RED + "You've somehow ended up with an invalid command!");
+	public boolean onCommand(CommandSender sender, Command command, String lbl, String[] args) {
+		sender.sendMessage(ChatColor.RED + lbl + " is not a valid command!");
 		return true;
 	}
-
 }
